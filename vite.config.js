807b2cc1c41cfc01e5f157e909094bb63943e56d
Sweta Katrodiya <swetakatrodiya.tech@gmail.com>
2026-03-04 15:39:17 +0530

    update push

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+  base: "/SWETA_KATRODIYA_PORTFOLIO/",
+});
